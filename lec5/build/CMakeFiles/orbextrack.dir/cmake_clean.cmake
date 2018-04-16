57cc0459f2265eb5556b7c2e0b73fb2b1d757ce8
jylhaust <jylhaust@163.com>
2018-04-16 20:01:41 +0800

    update

---
+++ lec5/build/CMakeFiles/orbextrack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbextrack.dir/computeORB.cpp.o"
+  "orbextrack.pdb"
+  "orbextrack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbextrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
